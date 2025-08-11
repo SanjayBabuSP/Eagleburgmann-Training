ec182d6c909f62fd58bd2952849476a465b559ee
sanjaybabu.suresh <sanjaybabu.suresh@eagleburgmann.com>
2025-08-11 08:53:55 +0000

    Pending mta file changes

--- training/app/orders/dist/ext/main/Main-dbg.controller.js
+++ training/app/orders/dist/ext/main/Main-dbg.controller.js
@@ -1,18 +1,10 @@
-sap.ui.define(["sap/fe/core/PageController", "sap/ui/model/odata/v4/ODataModel"], function (Controller, ODataModel) {
+sap.ui.define(["sap/fe/core/PageController"], function (Controller) {
   "use strict";
 
   /**
    * @namespace eagleburgmann.training.orders.ext.main
    */
-  const Main = Controller.extend("eagleburgmann.training.orders.ext.main.Main", {
-    onInit: function _onInit() {
-      const view = this.getView();
-      const oDataModel = new ODataModel({
-        serviceUrl: "/odata/v4/Orders/"
-      });
-      view.setModel(oDataModel);
-    }
-  });
+  const Main = Controller.extend("eagleburgmann.training.orders.ext.main.Main", {});
   return Main;
 });
 //# sourceMappingURL=Main-dbg.controller.js.map
--- training/app/orders/dist/test/integration/pages/OrdersMain.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/test/TemplatePage"],function(e){"use strict";var n={actions:{},assertions:{}};return new e("eagleburgmann.training.orders::OrdersMain",n)});
-//# sourceMappingURL=OrdersMain.js.map
